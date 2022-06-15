021cd59053d0665061f21af8e37a127fe66e1f10
Rolando Iván <69697573+rolandoivan@users.noreply.github.com>
2022-06-15 01:45:56 -0500

    reportar objetos Update

--- front/src/app/reportar-objetos-component/reportar-objetos-component.component.spec.ts
+++ front/src/app/reportar-objetos-component/reportar-objetos-component.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ReportarObjetosComponentComponent } from './reportar-objetos-component.component';
+
+
 
 describe('ReportarObjetosComponentComponent', () => {
   let component: ReportarObjetosComponentComponent;
