d0608dd752bf821179e5f2d96e337b9675eecc3d
Rolando Iván <69697573+rolandoivan@users.noreply.github.com>
2022-06-15 01:46:50 -0500

    reportar objetos Update

--- front/src/app/reportar-objetos-component/reportar-objetos-component.component.ts
+++ front/src/app/reportar-objetos-component/reportar-objetos-component.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reportar-objetos-component',
@@ -21,4 +21,23 @@ export class ReportarObjetosComponentComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  colorControl = new FormControl('primary');
+  fontSizeControl = new FormControl(16, Validators.min(10));
+  options = this.formBuilder.group({
+    color: this.colorControl,
+    fontSize: this.fontSizeControl,
+  });
+
+  getFontSize() {
+    return Math.max(10, this.fontSizeControl.value || 0);
+  }
+  email = new FormControl('', [Validators.required]);
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'Debes ingresar una categoria';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
 }
