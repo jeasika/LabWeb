321ee0a55a7e43099e6b71afcb2bea2813d605e6
Pamela Lozano <pamelalozano16@gmail.com>
2022-06-16 18:01:44 -0700

    Post object form and get object list

--- front/src/app/objetos-perdidos-component/objetos-perdidos-component.component.ts
+++ front/src/app/objetos-perdidos-component/objetos-perdidos-component.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AfterContentInit, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-objetos-perdidos-component',
   templateUrl: './objetos-perdidos-component.component.html',
-  styleUrls: ['./objetos-perdidos-component.component.css']
+  styleUrls: ['./objetos-perdidos-component.component.css'],
 })
 export class ObjetosPerdidosComponentComponent implements OnInit {
-
-  constructor() { }
-
+  private totalAngularPackages: any = [];
+  constructor(private http: HttpClient) {}
   ngOnInit(): void {
+    this.http
+      .get<any>('http://localhost:8000/api/objects/list', {
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+        },
+      })
+      .subscribe((data) => {
+        console.log(data);
+        this.totalAngularPackages = data;
+      });
   }
-
 }
--- front/src/app/reportar-objetos-component/reportar-objetos-component.component.ts
+++ front/src/app/reportar-objetos-component/reportar-objetos-component.component.ts
@@ -1,40 +1,60 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-reportar-objetos-component',
   templateUrl: './reportar-objetos-component.component.html',
-  styleUrls: ['./reportar-objetos-component.component.css']
+  styleUrls: ['./reportar-objetos-component.component.css'],
 })
-
 export class ReportarObjetosComponentComponent implements OnInit {
-
-  constructor(private formBuilder: FormBuilder){}
+  constructor(private formBuilder: FormBuilder, private http: HttpClient) {}
 
   campus = new FormControl('', Validators.required);
-  categoria = new FormControl('', [Validators.required]);
+  category = new FormControl('', [Validators.required]);
+  location = new FormControl('', [Validators.required]);
 
   reportarForm = this.formBuilder.group({
     campus: new FormControl(),
-    categoria: new FormControl(),
-    comentarios: new FormControl(),
-  })
+    location: new FormControl(),
+    category: new FormControl(),
+    comments: new FormControl(),
+  });
 
   onFormSubmit(): void {
-    console.log(this.reportarForm.value);
-  } 
+    let currentDate = new Date();
+    let valuesToSend = {
+      ...this.reportarForm.value,
+      imageBase64: ' ', //TODO: SAVE UPLOADED IMAGE STRING
+      status: 'active',
+      dateFound: currentDate.toISOString().split('T')[0],
+    };
+    console.log(valuesToSend);
+    try {
+      this.http
+        .post<any>('http://localhost:8000/api/objects/create', valuesToSend)
+        .subscribe((data) => {
+          console.log(data);
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   ngOnInit(): void {}
-
 
   /*constructor(private formBuilder: FormBuilder){}
 
   reportarForm = this.formBuilder.group({
     campus:[''],
     ubicacion:[''],
-    categoria:[''],
+    category:[''],
     comentarios:['']
 
   })*/
-  
 }
